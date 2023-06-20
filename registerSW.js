669e0e744e45e912c6f57a50f452d723d2eb1d19
rick-chou <56244178+rick-chou@users.noreply.github.com>
2023-06-20 01:42:31 +0000

    Deploying to gh-pages from @ rick-chou/react-flowchart@4d400c019da476c83a67aa2efbdaf77f8b5d17ab 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-flowchart/sw.js', { scope: '/react-flowchart/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-toolkit/sw.js', { scope: '/react-toolkit/' })})}
